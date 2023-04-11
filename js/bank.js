1c98e6df901142837c954219868aab43b919d556
Zual Rana <freelancerzual@gmail.com>
2023-04-11 11:43:17 +0600

    withdraw done

--- js/bank.js
+++ js/bank.js
@@ -13,3 +13,20 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   const oldBalanceValue = parseInt(oldBalanceText.innerText);
   oldBalanceText.innerText = oldBalanceValue + newDeposit;
 });
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  // get the withdraw input value
+  const withdrawInput = document.getElementById("withdraw-input");
+  const newWithdrawValue = parseInt(withdrawInput.value);
+  // get the balance value
+  const oldBalanceText = document.getElementById("balance");
+  const oldBalanceValue = parseInt(oldBalanceText.innerText);
+  // set the new value in balance
+  const newBalance = oldBalanceValue - newWithdrawValue;
+  oldBalanceText.innerText = newBalance;
+
+  // set the withdraw money in withdraw
+  const oldWithdrawText = document.getElementById("withdraw");
+  const oldWithdrawValue = parseInt(oldWithdrawText.innerText);
+  oldWithdrawText.innerText = oldWithdrawValue + newWithdrawValue;
+  withdrawInput.value = "";
+});
